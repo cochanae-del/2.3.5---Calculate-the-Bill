d53ca00a54f43a5117760e917504a30d90dbfed5
cochanae-del <co.chanae@gmail.com>
2026-06-05 03:05:48 +0000

    Calculate the bill.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function calculateBillTotal(subtotal)  {
+   var salesTax = .095 * subtotal;
+   var tip = .20 * subtotal;
+   
 
+   return  subtotal + salesTax + tip;
+      
+}
+console.log(calculateBillTotal(20));
